ca61c57ad30c1c377ab4809595619082515bd4fa
ashkan semsar <ashkansemsar@hotmail.com>
2022-11-27 16:29:42 +0330

    base entity

--- Domain/Entities/Conference.cs
+++ Domain/Entities/Conference.cs
@@ -1,7 +1,28 @@
-﻿namespace Domain.Entities;
+﻿using Domain.Enums;
+using Domain.Primitives;
 
-public class Conference
+namespace Domain.Entities;
+
+public class Conference:BaseEntity
 {
-    public Member Creator { get; set; }
+    public Conference(Guid id) : base(id)
+    {
+    }
+    public Member Creator { get; private set; }
 
+    public ConferenceType Type { get; private set; }
+
+    public string Name { get; private set; }
+
+    public DateTime ScheduledAtUtc { get; private set; }
+
+    public string? Location { get; private set; }
+
+    public int? MaximumNumberOfAttendees { get; private set; }
+
+    public DateTime? InvitationsExpireAtUtc { get; private set; }
+
+    public int NumberOfAttendees { get; private set; }
+
+    
 }
